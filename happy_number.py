bf0a3aa2341d091ffb0dd385916bf1f5b34387a5
Aswin J <aswinj271@gmail.com>
2024-11-29 22:46:38 +0530

    Happy Number

---
+++ happy_number.py
@@ -0,0 +1,22 @@
+def is_happy_number(n):
+    def sum_of_squares(num):
+        return sum(int(digit) ** 2 for digit in str(num))
+    
+    seen_numbers = set()  # To track numbers we've already seen
+    
+    while n != 1:
+        n = sum_of_squares(n)
+        
+        if n in seen_numbers:
+            return False  # Cycle detected, hence not a happy number
+        
+        seen_numbers.add(n)  # Add the current number to the set of seen numbers
+    
+    return True
+
+# Example usage:
+num = int(input("Enter a number: "))
+if is_happy_number(num):
+    print(f"{num} is a happy number!")
+else:
+    print(f"{num} is not a happy number.")
